155758063e29b9c77529f5b20f3d147a0cbd6636
JatinSachdeva2024 <jatinsachdeva@loyalistcollege.com>
2025-07-21 01:20:46 -0400

    updated login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -8,7 +8,7 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/com
 import { signIn, signOut, useSession } from 'next-auth/react';
 import Link from "next/link";
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function LoginPage() {
   const navLinks = [
@@ -21,6 +21,12 @@ export default function LoginPage() {
   const [error, setError] = useState('');
   const [loading, setLoading] = useState(false);
   const router = useRouter();
+
+  useEffect(() => {
+    if (status === "authenticated") {
+      router.replace("/dashboard"); // or "/"
+    }
+  }, [status, router]);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
