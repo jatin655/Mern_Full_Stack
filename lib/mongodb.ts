df2aee4f0fa3a13f3acd02536c9ff78d224bd5db
JatinSachdeva2024 <jatinsachdeva@loyalistcollege.com>
2025-07-21 02:08:27 -0400

    updated mongodb.ts

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,28 +1,25 @@
 import { MongoClient } from 'mongodb';
 
 const uri = process.env.MONGODB_URI;
+const options = {};
 
-let client: MongoClient;
+let client;
 let clientPromise: Promise<MongoClient>;
 
-if (!uri) {
-  console.warn('MONGODB_URI environment variable is not set. Some features may not work.');
-  // Create a dummy promise that rejects to prevent crashes
-  clientPromise = Promise.reject(new Error('MONGODB_URI not configured'));
-} else {
-  if (process.env.NODE_ENV !== 'production') {
-    // @ts-ignore
-    if (!global._mongoClientPromise) {
-      client = new MongoClient(uri);
-      // @ts-ignore
-      global._mongoClientPromise = client.connect();
-    }
-    // @ts-ignore
-    clientPromise = global._mongoClientPromise;
-  } else {
-    client = new MongoClient(uri);
-    clientPromise = client.connect();
-  }
+if (!process.env.MONGODB_URI) {
+  throw new Error('Please add your Mongo URI to .env.local');
 }
+
+declare global {
+  // allow global `var` declarations
+  // eslint-disable-next-line no-var
+  var _mongoClientPromise: Promise<MongoClient> | undefined;
+}
+
+if (!global._mongoClientPromise) {
+  client = new MongoClient(uri!, options);
+  global._mongoClientPromise = client.connect();
+}
+clientPromise = global._mongoClientPromise;
 
 export default clientPromise; 
